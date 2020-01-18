0d57945281375a128ac7333edc57b0f6d7812269
Abhi <18308855+SudoSharma@users.noreply.github.com>
2020-01-18 01:11:29 -0800

    [add] finish first page of react app

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,15 @@
 import React from "react"
+import Greeting from "../components/greeting.js"
+import "typeface-montserrat"
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Container, Row, Col } from "react-bootstrap"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <Container>
+    <Row>
+      <Col className="greeting-col">
+        <Greeting text="How are you feeling today?" />
+      </Col>
+    </Row>
+  </Container>
+)
